0d91b9d0ae28baa19d04aef5b8bc80df1c0b8821
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-07-20 16:43:25 +0900

    split json to another file

--- main.tf
+++ main.tf
@@ -21,77 +21,10 @@ terraform {
 }
 
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  definition    = jsonencode(
+  definition    = templatefile(
+    "definition.tftpl",
     {
-      Comment = "A Hello World example demonstrating various state types of the Amazon States Language"
-      StartAt = "Pass"
-      States  = {
-        "Hello World"          = {
-          End  = true
-          Type = "Pass"
-        }
-        "Hello World example?" = {
-          Choices = [
-            {
-              BooleanEquals = true
-              Next          = "Yes"
-              Variable      = "$.IsHelloWorldExample"
-            },
-            {
-              BooleanEquals = false
-              Next          = "No"
-              Variable      = "$.IsHelloWorldExample"
-            },
-          ]
-          Comment = "A Choice state adds branching logic to a state machine. Choice rules can implement 16 different comparison operators, and can be combined using And, Or, and Not"
-          Default = "Yes"
-          Type    = "Choice"
-        }
-        No                     = {
-          Cause = "Not Hello World"
-          Type  = "Fail"
-        }
-        "Parallel State"       = {
-          Branches = [
-            {
-              StartAt = "Hello"
-              States  = {
-                Hello = {
-                  End  = true
-                  Type = "Pass"
-                }
-              }
-            },
-            {
-              StartAt = "World"
-              States  = {
-                World = {
-                  End  = true
-                  Type = "Pass"
-                }
-              }
-            },
-          ]
-          Comment  = "A Parallel state can be used to create parallel branches of execution in your state machine."
-          Next     = "Hello World"
-          Type     = "Parallel"
-        }
-        Pass                   = {
-          Comment = "A Pass state passes its input to its output, without performing work. Pass states are useful when constructing and debugging state machines."
-          Next    = "Hello World example?"
-          Type    = "Pass"
-        }
-        "Wait 3 sec"           = {
-          Comment = "A Wait state delays the state machine from continuing for a specified time."
-          Next    = "Parallel State"
-          Seconds = 3
-          Type    = "Wait"
-        }
-        Yes                    = {
-          Next = "Wait 3 sec"
-          Type = "Pass"
-        }
-      }
+      wait_sec = 5
     }
   )
   name          = "HelloWorld"
